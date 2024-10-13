3138707fb27d79434499085945f9c42001be774e
sanderson <cbsanderson.04@gmail.com>
2024-10-13 18:27:41 -0300

    Add menu dropdown

---
+++ src/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { FaAngleDown, FaAnglesDown } from "react-icons/fa6";
+import styles from "./styles.module.css";
+
+const genres = [
+  "Ação",
+  "Comédia",
+  "Drama",
+  "Terror",
+  "Ficção Científica",
+  "Romance",
+  "Documentário"
+];
+
+export default function Dropdown({ onGenreSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState("Gênero");
+
+  function handleToggleDropdown() {
+    setIsOpen(!isOpen);
+  };
+
+  function handleSelectGenre(genre) {
+    setSelectedGenre(genre);
+    setIsOpen(false);
+    onGenreSelect(genre);
+  };
+
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.dropdownButton} onClick={handleToggleDropdown}>
+        {selectedGenre} <FaAngleDown />
+      </button>
+      {isOpen && (
+        <ul className={styles.dropdownMenu}>
+          {genres.map((genre) => (
+            <li
+              key={genre}
+              className={styles.dropdownItem}
+              onClick={() => handleSelectGenre(genre)}
+            >
+              {genre}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
