3a390324db34095e6bdd196324a5ff82a877b1fb
sanderson <cbsanderson.04@gmail.com>
2024-11-26 11:12:13 -0300

    wip: page with films watched and statistics

---
+++ src/pages/Movies/index.jsx
@@ -0,0 +1,10 @@
+import Menu from '../../components/Menu/Menu';
+
+export default function Movies() {
+    return (
+        <div>
+            <Menu />
+            <h1>Moviess</h1>
+        </div>
+    )
+}
